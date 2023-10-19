ca382da7cf390397ebae53bb2b5044dce48c455a
ecukier <ephram.cukier@colorado.edu>
2023-10-19 17:50:29 -0600

    basic buy demo

--- buy_demo.py
+++ buy_demo.py
@@ -2,18 +2,54 @@ import yfinance as yf
 import time
 
 import json
-import pandas as pd
+import os
+import time
 
-# ticker = input("Ticker: ")
-ticker = "GOOG"
+def empty_ticker_history():
+    return {
+        'curr_qty': 0,
+        'curr_avg_price': 0,
+        'history': []
+    }
 
-data = json.load(open('Data/data.json'))
+
+data_file = 'Data/data.json'
+data = {}
 
 
-while(True):
-    time.sleep(1)
+def write_data():
+    with open(data_file, "w") as write_file:
+        json.dump(data, write_file)
+
+
+if os.path.exists(data_file):
+    data = json.load(open(data_file))
+else:
+    print("Data file not found, creating")
+    write_data()
+
+while (True):
+    ticker = input("Ticker: ")
+    buy_qty = int(input("Buy Quantity: "))
+
+    if ticker not in data:
+        data[ticker] = empty_ticker_history()
+
     selected = yf.Ticker(ticker)
     finfo = selected.fast_info
-    print(f'{time.time()} {selected.ticker}: {finfo.last_price}')
+    new_price = finfo.last_price
 
+    #calucluate curr_avg_price using a weighted moving average
+    data[ticker]['curr_avg_price'] = (data[ticker]['curr_avg_price'] * data[ticker][
+        'curr_qty'] + buy_qty * new_price) / (data[ticker]['curr_qty'] + buy_qty)
+    data[ticker]['curr_qty'] += buy_qty
 
+    trade = {
+        'size': buy_qty,
+        'price': new_price,
+        'timestampt': time.time()
+    }
+
+    data[ticker]['history'].append(trade)
+
+    write_data()
