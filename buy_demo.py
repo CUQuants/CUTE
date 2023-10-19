98da25f3acf40f6983192ae0d31c41e6ec7a8c2a
effdotsh <ephram@eff.sh>
2023-10-19 17:25:39 -0600

    added data.json

--- buy_demo.py
+++ buy_demo.py
@@ -1,12 +1,19 @@
 import yfinance as yf
 import time
+
+import json
 import pandas as pd
 
 # ticker = input("Ticker: ")
 ticker = "GOOG"
+
+data = json.load(open('Data/data.json'))
+
 
 while(True):
     time.sleep(1)
     selected = yf.Ticker(ticker)
     finfo = selected.fast_info
     print(f'{time.time()} {selected.ticker}: {finfo.last_price}')
+
+
