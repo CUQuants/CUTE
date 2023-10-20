ef8ca44d7c85e8473c5c62640c133937595f2d03
ecukier <ephram.cukier@colorado.edu>
2023-10-19 18:08:06 -0600

    AssetManager class, buy implemented (untested)

---
+++ AssetManager.py
@@ -0,0 +1,52 @@
+import os
+import json
+import time
+
+class AssetManager():
+    def __int__(self, balance, data_file = 'Data/data.json'):
+        self.balance = balance
+        self.data_file = 'Data/data.json'
+        self.data = {}
+
+        if os.path.exists(data_file):
+            data = json.load(open(data_file))
+        else:
+            print("Data file not found, creating")
+            self.write_data()
+
+    def empty_ticker_history(self):
+        return {
+            'curr_qty': 0,
+            'curr_avg_price': 0,
+            'history': []
+        }
+
+    def write_data(self):
+        with open(self.data_file, "w") as write_file:
+            json.dump(self.data, write_file)
+
+    def buy(self, ticker, quantity):
+        if ticker not in self.data:
+            self.data[ticker] = self.empty_ticker_history()
+
+        selected = yf.Ticker(ticker)
+        finfo = selected.fast_info
+        new_price = finfo.last_price
+
+        # calucluate curr_avg_price using a weighted moving average
+        self.data[ticker]['curr_avg_price'] = (self.data[ticker]['curr_avg_price'] * self.data[ticker][
+            'curr_qty'] + quantity * new_price) / (self.data[ticker]['curr_qty'] + quantity)
+        self.data[ticker]['curr_qty'] += quantity
+
+        trade = {
+            'size': quantity,
+            'price': new_price,
+            'timestampt': time.time()
+        }
+
+        self.data[ticker]['history'].append(trade)
+
+        self.write_data()
+
+    def sell(self):
+        #TODO
