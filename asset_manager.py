c298e181f7b8b9afec7a1deb1d300998918a36dd
Ryan Watts <59616810+GoneInactive@users.noreply.github.com>
2023-10-19 19:13:34 -0600

    Update and rename AssetManager.py to asset_manager.py

--- asset_manager.py
+++ asset_manager.py
@@ -48,5 +48,22 @@ class AssetManager():
 
         self.write_data()
 
-    def sell(self):
-        #TODO
+    def sell(self, quantity, str, holdings, balance):
+        #def sell(quantity: float, ticker: str, holdings: float, balance: float):
+        yf_ticker = yf.Ticker(ticker)
+        data = yf_ticker.history()
+        last_quote = data['Close'].iloc[-1]
+        #print(ticker, last_quote)
+    
+        new_holdings = holdings - quantity
+    
+        if (new_holdings < 0):
+            return holdings, balance
+        
+        balance += quantity * last_quote
+    
+        return new_holdings, balance
+    
+        # print(f"Old holdings and balance: {(holdings, balance)}")
+        
+        # print(f"New holdings and balance: {sell(2, 'GOOG', holdings, balance)}")
