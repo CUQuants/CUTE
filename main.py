e66bc09c42c52feb15205c693505c9d5df3e95a3
Ryan Watts <59616810+GoneInactive@users.noreply.github.com>
2023-11-10 12:21:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from AssetManager import AssetManager as am
+from AssetManager import AssetManager
 
 def main():
   print(f'CU Quants Trading Emporium')
+  manager = AssetManager()
 
 if __name__ == "__main__":
     __main__()
