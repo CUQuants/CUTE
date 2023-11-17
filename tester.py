f947cd511167a368f9dd421d54db3c3b27e8ad72
ecukier <ephram.cukier@colorado.edu>
2023-11-16 18:04:18 -0700

    working buy/sell

--- tester.py
+++ tester.py
@@ -2,5 +2,11 @@ from AssetManager import AssetManager
 
 if __name__ == '__main__':
     aman = AssetManager(2000)
+    print(aman.balance)
+
     aman.buy('GOOG', 10)
     print(aman.balance)
+    aman.sell('GOOG', 5)
+    print(aman.balance)
+    # aman.sell('GS', 10)
+
