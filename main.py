db0321e763c03e2db74503dfd4eff453db94dbb8
Ryan Watts <59616810+GoneInactive@users.noreply.github.com>
2023-11-21 12:30:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from AssetManager import AssetManager
+from Analytics import Analytics
 
 def main():
   print(f'CU Quants Trading Emporium')
   manager = AssetManager()
+  metric = Analytics()
 
 if __name__ == "__main__":
     __main__()
