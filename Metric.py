f56614328f0ed0ffead2256932e4277f58c3de6e
Ryan Watts <59616810+GoneInactive@users.noreply.github.com>
2023-11-29 12:08:12 -0700

    Update and rename Analytics.py to Metric.py

--- Metric.py
+++ Metric.py
@@ -1,8 +1,6 @@
-class Analytics:
+class Metric:
   def __init___(self, ticker):
     self.ticker = ticker
 
   def get_pnl(self, intil_price, curr_price):
-    """
-    """
-    return
+    return curr_price/initl_price
