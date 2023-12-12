a6eff4f13d8a2e1c0d0d1908d537487e3f114912
Ryan Watts <59616810+GoneInactive@users.noreply.github.com>
2023-12-12 16:14:25 -0700

    Update Metric.py

--- Metric.py
+++ Metric.py
@@ -1,9 +1,17 @@
 class Metric:
-  def __init___(self, ticker):
-    self.ticker = ticker
+    def __init__(self,balance):
+        self.balance = balance
 
-  def get_pnl(self, intil_price, curr_price):
-    return curr_price/initl_price
-
-
-manager.data['Balance']
+    def pnl(self,curr_price,init_price,pnl_type='DOLLAR'):
+        if pnl_type.upper() == 'DOLLAR':
+            return curr_price-init_price
+        
+        elif pnl_type.upper() == 'PERCENT':
+            return (curr_price/init_price)*100
+        
+        print("!ERROR Invalid PNL Type For portfolio_pnl())")
+        return 0
+    
+    def portfolio_pnl(self,init_bal=2000,bal_type = 'DOLLAR'):
+        return self.pnl(self.balance,init_bal)
+    
