854811972f14d43aca3dd9b9e0886e6b694a73ff
effdotsh <ephram@eff.sh>
2024-10-29 18:03:09 -0600

    some untested setup code

--- python/CUTE.py
+++ python/CUTE.py
@@ -1,8 +1,38 @@
 import requests
-class Cute:
+
+class CUTE:
     def __init__(self, url: str = 'localhost:3000', step_interval_seconds: int = 60*5, start_time_unix: int = 1673560800):
         assert step_interval_seconds >= 60
-
+        self.version = '0.0.1'
         self.url = url
         self.step_interval_seconds = step_interval_seconds
         self.start_time_unix = start_time_unix
+
+
+    def buy(self, symbol: str, quantity: int):
+        assert quantity > 0
+        data = {
+            "symbol": symbol,
+            "quantity": quantity,
+        }
+        response = requests.post(self.url + '/buy', json=data)
+        if response.status_code != 200:
+            print(response.text)
+        return response
+
+    def sell(self, symbol: str, quantity: int):
+        assert quantity > 0
+        data = {
+            "symbol": symbol,
+            "quantity": quantity,
+        }
+        response = requests.post(self.url + '/sell', json=data)
+        if response.status_code != 200:
+            print(response.text)
+        return response
+
+    def step(self):
+        response = requests.post(self.url + '/step')
+        if response.status_code != 200:
+            print(response.text)
+        return response
--- python/example.py
+++ python/example.py
@@ -1,1 +1,5 @@
 # This example will buy/sell apple randomly every 5 minutes
+from CUTE import CUTE
+cute = CUTE(step_interval_seconds=5*60, #Each "step" should move the virtual timer forward 5 minutes. There is a minimum time of 1 minute (60 seconds), and each step gets rounded to the nearest minute, so multiples of 60 will work best.
+    start_time_unix=1673560800 #This is the unix timestamp for Jan 12 22:00:00 2023 UTC. Check out https://www.unixtimestamp.com/ for more info
+)
